b08cf7c01cb4938e27527144efde0df4089ebcd4
Terry <tchan247@gmail.com>
2015-10-11 11:37:50 -0700

    (fix) deploy test 2

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var io = require('socket.io');
 
 var app = express();
-var devPort = 3000;
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/client'));
 
-app.listen(process.env.PORT || devPort, function(){
-  console.log('listening to ' + (process.env.PORT || devPort));
+app.listen(port, function(){
+  console.log('listening to ' + port);
 });
