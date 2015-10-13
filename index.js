a3e22d327610d01dbfe65b70be8ee85749cf2308
Terry <tchan247@gmail.com>
2015-10-12 21:34:35 -0700

    (fix) deploy test 3

--- index.js
+++ index.js
@@ -7,5 +7,21 @@ var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/client'));
 
 app.listen(port, function(){
-  console.log('listening to ' + port);
+  console.log('listening to port: ' + port);
 });
+
+
+/**
+* TCP
+*/
+
+var net = require('net');
+var conn = net.createServer(function(socket) {
+
+  socket.write('Welcome to the GungHo test chat server');
+}).listen(3001);
+
+conn.on('connection', function(){
+  console.log('Connection :: ready');
+});
+
